430bb3675c9c4ee7ddd589e89d63f5dc41c17221
Francisco0901 <92781053+Francisco0901@users.noreply.github.com>
2021-10-19 03:55:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programacion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("probando cambio");
+        }
+    }
+}
