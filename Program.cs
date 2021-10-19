48e26fab108a9adc31eebd7b0b284428affdfef1
Francisco0901 <92781053+Francisco0901@users.noreply.github.com>
2021-10-19 03:55:55 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace programacion_1
+namespace proyecto_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("probando cambio");
+            int a;
+            a = 6;
+            int b;
+            b = 9;
+            int c;
+            c = 4;
+            Console.WriteLine("secuencias bca, aacb y bcbc de a=6, b=9 y c=4");
+            //bca
+            Console.WriteLine("primera secuencia:");
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+            //aacb
+            Console.WriteLine("segunda secuencia");
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(c);
+            Console.WriteLine(b);
+            //bcbc
+            Console.WriteLine("tercera secuencia");
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.Write(c);
         }
     }
 }
